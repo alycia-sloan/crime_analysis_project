828c4e844eeb164316f17103f9be37093d27b0a5
alycia-sloan <alycia.l.sloan@und.edu>
2024-08-28 13:50:34 -0400

    Month stats added
    Added some basic stats regarding crimes committed by month

--- data_explore.py
+++ data_explore.py
@@ -6,6 +6,7 @@ df_arrestee = pd.read_csv("data\\NIBRS_ARRESTEE.csv")
 df_offense_type = pd.read_csv("data\\NIBRS_OFFENSE_TYPE.csv")
 df_incident = pd.read_csv('data\\NIBRS_incident.csv')
 df_agencies = pd.read_csv('data\\agencies.csv')
+df_months = pd.read_csv('data\\NIBRS_month.csv')
 
 merge_arrestee_offtyp = pd.merge(df_arrestee, df_offense_type)
 
@@ -18,3 +19,12 @@ incident_count = merge_arrestee_offtyp['offense_category_name'].value_counts()
 incident_agency_merge = pd.merge(df_incident,df_agencies)
 incident_agency_count = incident_agency_merge['county_name'].value_counts()
 #print(incident_agency_count)
+
+
+month_incident_merge = pd.merge(df_incident, df_months)
+month_incident_count = month_incident_merge['month_num'].value_counts()
+sorted_month_incident_count = month_incident_count.sort_index()
+print(sorted_month_incident_count)
+print("--Monthly Crime Stats--")
+print(f"Average: {sorted_month_incident_count.mean()}")
+print(f"Standard Deviation: {sorted_month_incident_count.std()}")
